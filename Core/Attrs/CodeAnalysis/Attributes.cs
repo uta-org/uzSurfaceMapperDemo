bc18b386c023fa211d828cab3fbb91e103c5f5c9
z3nth10n <z3nth10n@gmail.com>
2020-10-15 01:45:24 +0200

    add plugins

---
+++ Core/Attrs/CodeAnalysis/Attributes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace uzSurfaceMapper.Core.Attrs.CodeAnalysis
+{
+    /// <summary>
+    ///     The code with this attribute must be reviewed
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class MustBeReviewed : Attribute
+    {
+    }
+
+    /// <summary>
+    ///     The code with this attribute is bugged
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class Bugged : Attribute
+    {
+    }
+
+    /// <summary>
+    ///     The code with this attribute is in (work in progress)
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class WIP : Attribute
+    {
+    }
+
+    /// <summary>
+    ///     The code with this attribute is only for debug purpouses
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnlyDebug : Attribute
+    {
+    }
+
+    /// <summary>
+    ///     The code with this attribute is being tested or part of a test
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class Testing : Attribute
+    {
+    }
+
+    /// <summary>
+    ///     The code with this attribute needs to be changed
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class ChangeCode : Attribute
+    {
+    }
+
+    /// <summary>
+    ///     The code with this attribute isn't recommended to be used
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.All)]
+    public class NotRecommended : Attribute
+    {
+    }
+}
---
+++ Keyboard/MultiTapper.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+namespace uzSurfaceMapper.Utils.Keyboard
+{
+    /// <summary>
+    ///     Checks multi key tapping from Keyboard
+    /// </summary>
+    public class MultiTapper
+    {
+        /// <summary>
+        ///     The key tap delay
+        /// </summary>
+        private const float keyTapDelay = .5f;
+
+        /// <summary>
+        ///     The button cooler
+        /// </summary>
+        private float ButtonCooler;
+
+        /// <summary>
+        ///     The button count
+        /// </summary>
+        private int ButtonCount;
+
+        /// <summary>
+        ///     Prevents a default instance of the <see cref="MultiTapper" /> class from being created.
+        /// </summary>
+        private MultiTapper()
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MultiTapper" /> class.
+        /// </summary>
+        /// <param name="keyCode">The key code.</param>
+        /// <param name="count">The count.</param>
+        public MultiTapper(KeyCode keyCode, int count = 2)
+        {
+            KeyCode = keyCode;
+            Count = count;
+        }
+
+        /// <summary>
+        ///     Gets or sets the key code.
+        /// </summary>
+        /// <value>
+        ///     The key code.
+        /// </value>
+        public KeyCode KeyCode { private get; set; }
+
+        public int Count { private get; set; }
+
+        /// <summary>
+        ///     Checks the multi tap.
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckMultiTap()
+        {
+            if (Input.GetKeyDown(KeyCode))
+            {
+                if (ButtonCooler > 0 && ButtonCount == Count - 1)
+                {
+                    return true;
+                }
+
+                ButtonCooler = keyTapDelay;
+                ButtonCount += 1;
+            }
+
+            if (ButtonCooler > 0)
+                ButtonCooler -= 1 * Time.deltaTime;
+            else
+                ButtonCount = 0;
+
+            return false;
+        }
+    }
+}
