59c8a192aa09f4c8b53ba9a6d96ca3430cb3f5d2
z3nth10n <z3nth10n@gmail.com>
2021-01-21 01:27:15 +0100

    no errors with uzsurfacemapperdemo unity project

---
+++ Extensions/MathHelper.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_2020 || UNITY_2019 || UNITY_2018 || UNITY_2017 || UNITY_5
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+//using uzLib.Lite.Extensions;
+
+namespace UnityEngine.Extensions
+{
+    /// <summary>
+    /// The MathHelper class
+    /// </summary>
+    public static class MathHelper
+    {
+        /// <summary>Get the multiples the of.</summary>
+        /// <param name="value">The value.</param>
+        /// <param name="multipleOf">The multiple to round off.</param>
+        /// <returns></returns>
+        public static float MultipleOf(this float value, float multipleOf)
+        {
+            return Mathf.Round(value / multipleOf) * multipleOf;
+        }
+    }
+}
+
+#endif
